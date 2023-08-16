c1cb562083be1ee695d2170286346c06c8666281
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-16 14:57:12 -0400

    imported Form component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import styles from './layout.module.css';
 import Sidebar from '../sidebar/Sidebar.jsx';
+import Form from '../form/Form.jsx';
 
 const Layout = () => {
   return (
     <div className={styles.container}>
       <Sidebar />
+      <Form />
     </div>
   );
 };
