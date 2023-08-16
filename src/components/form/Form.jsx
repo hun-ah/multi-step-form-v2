bb66dc80de2c2300f2e64fa01aa2ac7eceb56141
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-16 14:57:40 -0400

    created component

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,11 @@
+import styles from './form.module.css';
+
+const Form = () => {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}></form>
+    </div>
+  );
+};
+
+export default Form;
