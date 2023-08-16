c3731f50da1434275b11335a83baeb78281f2405
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-16 19:25:34 -0400

    imported form components and created index state

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,9 +1,23 @@
 import styles from './form.module.css';
+import { useState } from 'react';
+import PersonalInfo from '../personalInfo/PersonalInfo';
+import SelectPlan from '../selectPlan/SelectPlan';
+import AddOns from '../addOns/AddOns.jsx';
+import FinishingUp from '../finishingUp/FinishingUp.jsx';
+import ThankYou from '../thankYou/ThankYou.jsx';
 
 const Form = () => {
+  const [index, setIndex] = useState(1);
   return (
     <div className={styles.container}>
-      <form className={styles.form}></form>
+      <form className={styles.form}>
+        {index === 1 && <PersonalInfo />}
+        {index == 2 && <SelectPlan />}
+        {index == 3 && <AddOns />}
+        {index == 4 && <FinishingUp />}
+        {index == 5 && <ThankYou />}
+      </form>
+      <div className={styles.buttonContainer}></div>
     </div>
   );
 };
