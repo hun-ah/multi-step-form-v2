b254fe83e41b50343eff85fc47b7040df13b0963
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-17 11:54:36 -0400

    spaced variable from return statement

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,10 +1,17 @@
 import styles from './button.module.css';
 
-const Button = ({ text, index, nextStep, prevStep }) => {
+const Button = ({ text, index, id, nextStep, prevStep }) => {
+  const btnStyle =
+    id == 'back'
+      ? `${styles.back}`
+      : id == 'confirm'
+      ? `${styles.button} ${styles.confirm}`
+      : `${styles.button}`;
+
   return (
     <button
-      className={index == 1 ? styles.hide : styles.button}
-      onClick={text === 'Next Step' ? nextStep : prevStep}
+      className={index == 1 ? styles.hide : btnStyle}
+      onClick={id == 'next' ? nextStep : prevStep}
     >
       {text}
     </button>
