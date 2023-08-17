ac3696c00f10a68bebb61875b4be1b497ee20565
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-17 15:37:25 -0400

    moved index state up and passed to components in Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,15 @@
 import styles from './layout.module.css';
+import { useState } from 'react';
 import Sidebar from '../sidebar/Sidebar.jsx';
 import Form from '../form/Form.jsx';
 
 const Layout = () => {
+  const [index, setIndex] = useState(1);
+
   return (
     <div className={styles.container}>
-      <Sidebar />
-      <Form />
+      <Sidebar index={index} />
+      <Form index={index} setIndex={setIndex} />
     </div>
   );
 };
