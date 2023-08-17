03ebb68530b4cbe3235c2c0dbdf1c0f5bcc9219a
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-17 12:43:17 -0400

    logic for displaying Button component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -5,9 +5,24 @@ import SelectPlan from '../selectPlan/SelectPlan';
 import AddOns from '../addOns/AddOns.jsx';
 import FinishingUp from '../finishingUp/FinishingUp.jsx';
 import ThankYou from '../thankYou/ThankYou.jsx';
+import Button from '../button/Button';
 
 const Form = () => {
   const [index, setIndex] = useState(1);
+  const nextStep = () => {
+    setIndex((prevIndex) => {
+      if (prevIndex === 5) {
+        return prevIndex;
+      } else {
+        return prevIndex + 1;
+      }
+    });
+  };
+
+  const prevStep = () => {
+    setIndex((prevIndex) => prevIndex - 1);
+  };
+
   return (
     <div className={styles.container}>
       <form className={styles.form}>
@@ -17,7 +32,14 @@ const Form = () => {
         {index == 4 && <FinishingUp />}
         {index == 5 && <ThankYou />}
       </form>
-      <div className={styles.buttonContainer}></div>
+      <div className={styles.buttonContainer}>
+        <Button text='Go Back' index={index} prevStep={prevStep} id='back' />
+        {index == 4 ? (
+          <Button text='Confirm' id='confirm' />
+        ) : (
+          <Button text='Next Step' nextStep={nextStep} id='next' />
+        )}
+      </div>
     </div>
   );
 };
