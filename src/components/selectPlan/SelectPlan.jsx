a0fcbacffe9db835da74df9258f08a8458aa6035
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-19 17:28:51 -0400

    added new variables and imported OptionLayout

--- src/components/selectPlan/SelectPlan.jsx
+++ src/components/selectPlan/SelectPlan.jsx
@@ -2,9 +2,37 @@
 import OptionLayout from '../optionLayout/OptionLayout.jsx';
 
 const SelectPlan = () => {
+  const componentName = 'select plan';
+
   const heading = 'Select your plan';
+
   const subheading = 'You have the option of monthly or yearly billing.';
-  return <OptionLayout heading={heading} subheading={subheading} />;
+
+  const inputs = [
+    {
+      type: 'radio',
+      label: 'Arcade',
+      id: 'arcade',
+    },
+    {
+      type: 'radio',
+      label: 'Advanced',
+      id: 'advanced',
+    },
+    {
+      type: 'radio',
+      label: 'Pro',
+      id: 'pro',
+    },
+  ];
+  return (
+    <OptionLayout
+      heading={heading}
+      subheading={subheading}
+      inputs={inputs}
+      component={componentName}
+    />
+  );
 };
 
 export default SelectPlan;
