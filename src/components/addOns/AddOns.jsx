8ac7ca75ada4da4abce50cd55dc8edd429c36105
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-19 17:32:17 -0400

    added new variables and imported OptionLayout

--- src/components/addOns/AddOns.jsx
+++ src/components/addOns/AddOns.jsx
@@ -3,8 +3,33 @@ import OptionLayout from '../optionLayout/OptionLayout.jsx';
 
 const AddOns = () => {
   const heading = 'Pick add-ons';
+
   const subheading = 'Add-ons help enhance your gaming experience';
-  return <OptionLayout heading={heading} subheading={subheading} />;
+
+  const inputs = [
+    {
+      type: 'checkbox',
+      label: 'Name',
+      placeholder: 'e.g. Stephen King',
+      id: 'name',
+    },
+    {
+      type: 'checkbox',
+      label: 'Email Address',
+      placeholder: 'e.g. stephenking@lorem.com',
+      id: 'email',
+    },
+    {
+      type: 'checkbox',
+      label: 'Phone Number',
+      placeholder: '123 567 8910',
+      id: 'phone',
+    },
+  ];
+
+  return (
+    <OptionLayout heading={heading} subheading={subheading} inputs={inputs} />
+  );
 };
 
 export default AddOns;
