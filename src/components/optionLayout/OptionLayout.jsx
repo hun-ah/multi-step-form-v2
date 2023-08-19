0b59adff4ccba7ca8a7317e2e907c68a6382d30a
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-19 17:29:38 -0400

    added logic for rendering specific component

--- src/components/optionLayout/OptionLayout.jsx
+++ src/components/optionLayout/OptionLayout.jsx
@@ -1,13 +1,39 @@
 import styles from './optionlayout.module.css';
+import Input from '../input/Input';
+import TogglePlan from '../togglePlan/TogglePlan.jsx';
 
-const OptionLayout = ({ heading, subheading }) => {
+const OptionLayout = ({ heading, subheading, inputs, component }) => {
+  let inputContainerStyle;
+  if (component == 'personal info') {
+    inputContainerStyle = `${styles.personalInfoContainer}`;
+  } else if (component == 'select plan') {
+    inputContainerStyle = `${styles.selectPlanContainer}`;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.headingContainer}>
         <h1 className={styles.heading}>{heading}</h1>
         <h2 className={styles.subheading}>{subheading}</h2>
       </div>
-      <div className={styles.inputContainer}></div>
+      <div
+        className={
+          inputContainerStyle ? inputContainerStyle : styles.inputContainer
+        }
+      >
+        {inputs &&
+          inputs.map((input) => (
+            <Input
+              key={Math.random()}
+              type={input.type}
+              label={input.label}
+              placeholder={input.placeholder}
+              id={input.id}
+              component={component}
+            />
+          ))}
+      </div>
+      {component == 'select plan' && <TogglePlan />}
     </div>
   );
 };
