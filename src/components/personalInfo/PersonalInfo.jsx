0b6df3b79e37ed958b1d68cbbd9b222d9ac993cb
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-19 17:29:08 -0400

    added new variables and imported OptionLayout

--- src/components/personalInfo/PersonalInfo.jsx
+++ src/components/personalInfo/PersonalInfo.jsx
@@ -2,11 +2,42 @@
 import OptionLayout from '../optionLayout/OptionLayout';
 
 const PersonalInfo = () => {
+  const componentName = 'personal info';
+
   const heading = 'Personal info';
+
   const subheading =
     'Please provide your name, email address and phone number.';
 
-  return <OptionLayout heading={heading} subheading={subheading} />;
+  const inputs = [
+    {
+      type: 'text',
+      label: 'Name',
+      placeholder: 'e.g. Stephen King',
+      id: 'name',
+    },
+    {
+      type: 'text',
+      label: 'Email Address',
+      placeholder: 'e.g. stephenking@lorem.com',
+      id: 'email',
+    },
+    {
+      type: 'text',
+      label: 'Phone Number',
+      placeholder: '123 567 8910',
+      id: 'phone',
+    },
+  ];
+
+  return (
+    <OptionLayout
+      heading={heading}
+      subheading={subheading}
+      inputs={inputs}
+      component={componentName}
+    />
+  );
 };
 
 export default PersonalInfo;
