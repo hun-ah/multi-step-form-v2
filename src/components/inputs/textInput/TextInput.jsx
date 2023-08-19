c6a79a79b47793d3c831803310c315b3219f10c5
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-19 18:02:29 -0400

    created component

--- src/components/inputs/textInput/TextInput.jsx
+++ src/components/inputs/textInput/TextInput.jsx
@@ -1,8 +1,17 @@
-const TextInput = () => {
+import styles from './input.module.css';
+
+const TextInput = ({ label, placeholder, id }) => {
   return (
-    <div>
-      <label htmlFor=''></label>
-      <input type='text' />
+    <div className={styles.container}>
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+      <input
+        type='text'
+        className={styles.input}
+        placeholder={placeholder}
+        id={id}
+      />
     </div>
   );
 };
