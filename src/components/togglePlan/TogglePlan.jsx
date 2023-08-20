6ed0223fb285001c351b213b8ab3558f3b4d1bbb
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-20 04:12:13 -0400

    added elements and styling

--- src/components/togglePlan/TogglePlan.jsx
+++ src/components/togglePlan/TogglePlan.jsx
@@ -1,5 +1,16 @@
+import styles from './toggleplan.module.css';
+
 const TogglePlan = () => {
-  return <div>toggle monthly/yearly</div>;
+  return (
+    <div className={styles.container}>
+      <span>Yearly</span>
+      <label className={styles.switch}>
+        <input type='checkbox' />
+        <span className={`${styles.slider} ${styles.round}`}></span>
+      </label>
+      <span>Monthly</span>
+    </div>
+  );
 };
 
 export default TogglePlan;
