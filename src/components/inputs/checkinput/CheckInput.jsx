f512556d62cbd4fdc3eb51b96cb0371883bb98a2
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-21 16:56:22 -0400

    added props and rest of elements to create component

--- src/components/inputs/checkinput/CheckInput.jsx
+++ src/components/inputs/checkinput/CheckInput.jsx
@@ -1,12 +1,16 @@
 import styles from './input.module.css';
 
-const CheckInput = ({ id, label }) => {
+const CheckInput = ({ id, label, text, monthlyPrice, yearlyPrice }) => {
   return (
     <div className={styles.container}>
       <label htmlFor={id} className={styles.label}>
-        {label}
+        <input type='checkbox' className={styles.input} id={id} />
+        <div className={styles.textContainer}>
+          <span className={styles.addon}>{label}</span>
+          <span className={styles.description}>{text}</span>
+        </div>
+        <span>+${monthlyPrice}/mo</span>
       </label>
-      <input type='checkbox' className={styles.input} id={id} />
     </div>
   );
 };
