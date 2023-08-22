88a173c1c5dc7c06fb0d5803b522a3559d0d053e
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-22 06:54:10 -0400

    import input context and added value, onChange to input element

--- src/components/inputs/textInput/TextInput.jsx
+++ src/components/inputs/textInput/TextInput.jsx
@@ -1,6 +1,10 @@
 import styles from './input.module.css';
+import { useContext } from 'react';
+import { InputContext } from '../../../contexts/InputContext';
 
-const TextInput = ({ label, placeholder, id }) => {
+const TextInput = ({ label, placeholder, id, name }) => {
+  const { formInputs, handleInputChange } = useContext(InputContext);
+
   return (
     <div className={styles.container}>
       <label htmlFor={id} className={styles.label}>
@@ -11,6 +15,9 @@ const TextInput = ({ label, placeholder, id }) => {
         className={styles.input}
         placeholder={placeholder}
         id={id}
+        name={name}
+        value={formInputs[name]}
+        onChange={handleInputChange}
       />
     </div>
   );
