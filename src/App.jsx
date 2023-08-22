7571e7bf2a1cedb34d177f0343b50dfe86005a51
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-22 06:52:07 -0400

    moved state, imported and created context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import Layout from './components/layout/Layout.jsx';
+import { useState } from 'react';
+import { InputContext } from './contexts/InputContext';
 
 function App() {
+  const [formInputs, setFormInputs] = useState({
+    name: '',
+    emailAddress: '',
+    phoneNumber: '',
+    planType: 'arcade',
+    monthlyPayment: true,
+    onlineService: false,
+    largerStorage: false,
+    customizeableProfile: false,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormInputs((prevInputs) => {
+      return {
+        ...prevInputs,
+        [name]: type === 'checkbox' ? checked : value,
+      };
+    });
+  };
+
   return (
     <>
-      <Layout />
+      <InputContext.Provider value={{ formInputs, handleInputChange }}>
+        <Layout />
+      </InputContext.Provider>
     </>
   );
 }
