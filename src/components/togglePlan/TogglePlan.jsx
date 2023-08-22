8b11be8fc66f1aca486cc97337dac86501d4cb13
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-22 16:38:30 -0400

    imported input context and logic for switching between yearly/monthly plan

--- src/components/togglePlan/TogglePlan.jsx
+++ src/components/togglePlan/TogglePlan.jsx
@@ -1,14 +1,35 @@
 import styles from './toggleplan.module.css';
+import { useContext } from 'react';
+import { InputContext } from '../../contexts/InputContext';
 
 const TogglePlan = () => {
+  const { formInputs, handleInputChange } = useContext(InputContext);
+
   return (
     <div className={styles.container}>
-      <span>Yearly</span>
+      <span
+        className={
+          formInputs.monthlyPayment ? `${styles.deselected}` : undefined
+        }
+      >
+        Yearly
+      </span>
       <label className={styles.switch}>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          name='monthlyPayment'
+          checked={formInputs.monthlyPayment}
+          onChange={handleInputChange}
+        />
         <span className={`${styles.slider} ${styles.round}`}></span>
       </label>
-      <span>Monthly</span>
+      <span
+        className={
+          !formInputs.monthlyPayment ? `${styles.deselected}` : undefined
+        }
+      >
+        Monthly
+      </span>
     </div>
   );
 };
