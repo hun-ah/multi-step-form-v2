cb00f65f4cf263839f6c8271883c1a9e114dd953
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-22 16:39:04 -0400

    imported input context and logic for input state

--- src/components/inputs/radioInput/RadioInput.jsx
+++ src/components/inputs/radioInput/RadioInput.jsx
@@ -2,19 +2,40 @@ import styles from './input.module.css';
 import { ReactComponent as ArcadeIcon } from '../../../assets/icon-arcade.svg';
 import { ReactComponent as AdvancedIcon } from '../../../assets/icon-advanced.svg';
 import { ReactComponent as ProIcon } from '../../../assets/icon-pro.svg';
+import { useContext } from 'react';
+import { InputContext } from '../../../contexts/InputContext';
 
-const RadioInput = ({ label, id, monthlyPrice, yearlyPrice }) => {
+const RadioInput = ({ label, id, name, value, monthlyPrice, yearlyPrice }) => {
+  const { formInputs, handleInputChange } = useContext(InputContext);
+  const labelStyle =
+    formInputs.planType == value
+      ? `${styles.labelContainer} ${styles.selected}`
+      : `${styles.labelContainer}`;
+
   return (
     <div className={styles.container}>
-      <label htmlFor={id} className={styles.labelContainer}>
+      <label htmlFor={id} className={labelStyle}>
         {id == 'arcade' && <ArcadeIcon />}
         {id == 'advanced' && <AdvancedIcon />}
         {id == 'pro' && <ProIcon />}
         <div className={styles.planPrice}>
           <span className={styles.label}>{label}</span>
-          <span className={styles.price}>+${monthlyPrice}/yr</span>
+          <span className={styles.price}>
+            ${monthlyPrice ? monthlyPrice : yearlyPrice}/yr
+          </span>
+          {!formInputs.monthlyPayment && (
+            <span className={styles.free}>2 months free</span>
+          )}
         </div>
-        <input type='radio' className={styles.input} id={id} />
+        <input
+          type='radio'
+          className={styles.input}
+          id={id}
+          name={name}
+          value={value}
+          checked={formInputs.planType == value}
+          onChange={handleInputChange}
+        />
       </label>
     </div>
   );
