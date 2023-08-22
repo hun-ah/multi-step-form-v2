6ac5e81bbeebd74fea12fa0a2430d9f0dd9777d4
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-22 16:54:30 -0400

    added name and onChange to input

--- src/components/inputs/checkinput/CheckInput.jsx
+++ src/components/inputs/checkinput/CheckInput.jsx
@@ -2,13 +2,19 @@ import styles from './input.module.css';
 import { useContext } from 'react';
 import { InputContext } from '../../../contexts/InputContext';
 
-const CheckInput = ({ id, label, text, monthlyPrice, yearlyPrice }) => {
+const CheckInput = ({ id, label, text, name, monthlyPrice, yearlyPrice }) => {
   const { formInputs, handleInputChange } = useContext(InputContext);
 
   return (
     <div className={styles.container}>
       <label htmlFor={id} className={styles.label}>
-        <input type='checkbox' className={styles.input} id={id} />
+        <input
+          type='checkbox'
+          className={styles.input}
+          id={id}
+          name={name}
+          onChange={handleInputChange}
+        />
         <div className={styles.textContainer}>
           <span className={styles.addon}>{label}</span>
           <span className={styles.description}>{text}</span>
