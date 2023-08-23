0a9edcb61da19d39353ec0c980b92639fb6d3010
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-23 06:09:30 -0400

    style for selected input and checked value for input

--- src/components/inputs/checkinput/CheckInput.jsx
+++ src/components/inputs/checkinput/CheckInput.jsx
@@ -5,14 +5,19 @@ import { InputContext } from '../../../contexts/InputContext';
 const CheckInput = ({ id, label, text, name, monthlyPrice, yearlyPrice }) => {
   const { formInputs, handleInputChange } = useContext(InputContext);
 
+  const labelStyle = formInputs[name]
+    ? `${styles.label} ${styles.selected}`
+    : `${styles.label}`;
+
   return (
     <div className={styles.container}>
-      <label htmlFor={id} className={styles.label}>
+      <label htmlFor={id} className={labelStyle}>
         <input
           type='checkbox'
           className={styles.input}
           id={id}
           name={name}
+          checked={formInputs[name]}
           onChange={handleInputChange}
         />
         <div className={styles.textContainer}>
