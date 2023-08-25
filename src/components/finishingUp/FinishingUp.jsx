fb9646c915425c7308a6785da2e7ea05c8636fbc
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-25 12:47:26 -0400

    added componentName variable and gave as prop to OptionLayout

--- src/components/finishingUp/FinishingUp.jsx
+++ src/components/finishingUp/FinishingUp.jsx
@@ -1,11 +1,19 @@
-// import styles from './finishingup.module.css';
 import OptionLayout from '../optionLayout/OptionLayout.jsx';
 
 const FinishingUp = () => {
+  const componentName = 'finishing up';
+
   const heading = 'Finishing up';
+
   const subheading = 'Double-check everything looks OK before confirming.';
 
-  return <OptionLayout heading={heading} subheading={subheading} />;
+  return (
+    <OptionLayout
+      heading={heading}
+      subheading={subheading}
+      component={componentName}
+    />
+  );
 };
 
 export default FinishingUp;
