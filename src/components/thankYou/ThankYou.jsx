d378245e92825008e4274c42ddbcc5b5089f7500
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-25 15:12:56 -0400

    finished component

--- src/components/thankYou/ThankYou.jsx
+++ src/components/thankYou/ThankYou.jsx
@@ -1,7 +1,22 @@
 import styles from './thankyou.module.css';
+import { ReactComponent as Thanks } from '../../assets/icon-thank-you.svg';
 
 const ThankYou = () => {
-  return <div className={styles.container}>Thanks</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>
+        <Thanks className={styles.icon} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.heading}>Thank you!</h1>
+        <p className={styles.p}>
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@loremgaming.com.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ThankYou;
