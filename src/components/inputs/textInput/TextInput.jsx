be8ba83014dc46574c8a4acf14db802702e557c7
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-27 09:05:55 -0400

    style and input handling for input errors

--- src/components/inputs/textInput/TextInput.jsx
+++ src/components/inputs/textInput/TextInput.jsx
@@ -2,22 +2,35 @@ import styles from './input.module.css';
 import { useContext } from 'react';
 import { InputContext } from '../../../contexts/InputContext';
 
-const TextInput = ({ label, placeholder, id, name }) => {
+const TextInput = ({ label, placeholder, id, name, errMsg, setErrMsg }) => {
   const { formInputs, handleInputChange } = useContext(InputContext);
+
+  const inputStyle = errMsg
+    ? `${styles.input} ${styles.error}`
+    : `${styles.input}`;
 
   return (
     <div className={styles.container}>
-      <label htmlFor={id} className={styles.label}>
-        {label}
-      </label>
+      <div className={styles.labelContainer}>
+        <label htmlFor={id} className={styles.label}>
+          {label}
+        </label>
+        {errMsg && <span className={styles.errorMsg}>{errMsg}</span>}
+      </div>
       <input
         type='text'
-        className={styles.input}
+        className={inputStyle}
         placeholder={placeholder}
         id={id}
         name={name}
         value={formInputs[name]}
-        onChange={handleInputChange}
+        onChange={() => {
+          handleInputChange(event);
+          setErrMsg((prevMsg) => ({
+            ...prevMsg,
+            [name]: '',
+          }));
+        }}
       />
     </div>
   );
