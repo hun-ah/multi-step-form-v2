59caa012ae0db9970f124c87f33e664f0549ffc0
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-08-27 21:25:22 -0400

    reverted to original headers code

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -52,17 +52,15 @@ const Form = ({ index, setIndex }) => {
   const submitForm = async () => {
     setIndex((prevIndex) => prevIndex + 1);
     const newClient = { ...formInputs };
-    const headers = {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS',
-    };
+
     try {
       const res = await fetch(
         'https://multi-step-form-api.vercel.app/submitUserInfo',
         {
           method: 'POST',
-          headers: headers,
+          headers: {
+            'Content-Type': 'application/json',
+          },
           body: JSON.stringify(newClient),
         }
       );
